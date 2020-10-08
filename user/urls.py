6c67d4fa3f4f725c1ecb02e887738d68ff6c83fc
Binamra <binamraprogrammer@gmail.com>
2020-10-08 11:56:32 +0545

    Added login and regsiter

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/', views.auth, name='auth'),
+    path('auth/', views.authentication, name='auth'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
 ]
