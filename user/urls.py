ae05f2a79f2a79bb5c2f5fccb0dbc12eaca01cbd
Binamra <binamraprogrammer@gmail.com>
2020-10-08 12:38:29 +0545

    Project idea to use django dropped. Now, idea is to use native apps

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('auth/', views.authentication, name='auth'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
-]
